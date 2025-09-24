e979827defcab34a95a7afb7e469ccd637e62854
Nitesh Meshram <meshram.nitesh@gmail.com>
2025-09-24 08:28:59 +0530

    Added GetCount and AddFirst

--- LinkList_Examples/LinkList.c
+++ LinkList_Examples/LinkList.c
@@ -11,18 +11,23 @@ struct Node* head = NULL;
 
 void append(int new_data);
 void display();
+int getCount();
+void addFirst(int item);
 
 
 int main() 
 {
-    struct  Node* head = (struct Node*)malloc(sizeof(struct Node));
+    // struct  Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head = (struct Node*)malloc(sizeof(struct Node));
 
     int ch, ele;
     while(1)
     {
         printf("1. Append \n");
-        printf("2 Display List: \n");
-        printf("3 Exis: \n");
+        printf("2. Display List: \n");
+        printf("3. GetCount: \n");
+        printf("4. AddFirst: \n");
+        printf("5. Exis: \n");
         printf("Enter your choice : ");
         scanf("%d", &ch);
         
@@ -35,6 +40,18 @@ int main()
         else if(ch==2)
         {
            display();
+        }
+         else if(ch==3)
+        {
+            int count = getCount();
+            printf("Total Item Count  %d: ",count);
+           
+        }
+         else if(ch==4)
+        {
+            printf("Enter ele to Add at First Position  : ");
+            scanf("%d", &ele);
+            addFirst(ele);
         }
          else if(ch==5)
         {
@@ -80,4 +97,21 @@ void display() {
         ptr = ptr->next;
     }
     printf("NULL\n");
+}
+
+int getCount() {
+    struct Node* current = head;
+    int count = 0;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+void addFirst(int item) {
+    struct Node* temp = (struct Node*) malloc(sizeof (struct Node));
+    temp->next = head;
+    temp->data = item;
+    head = temp;
 }
